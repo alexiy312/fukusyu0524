ba99bf6355102f5a11c33f93b7d3a002155fe91c
satoshigp <satoshi3104>
2019-05-24 13:16:38 +0900

    初期設定

--- fukusyu0524/Form1.cs
+++ fukusyu0524/Form1.cs
@@ -12,9 +12,42 @@ namespace fukusyu0524
 {
     public partial class Form1 : Form
     {
+        int vx = 0;
+        int vy = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Top += vx;
+            label1.Left += vy;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            vx = -5;
+            vy = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            vy = -5;
+            vx = 0;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            vy = 5;
+            vx = 0;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            vx = 5;
+            vy = 0;
         }
     }
 }
